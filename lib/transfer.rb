6bf78ce129607682be22616254de19dc76189101
Doug Schallmoser <dougschallmoser@mgail.com>
2020-07-20 23:22:02 -0700

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,22 +16,39 @@ class Transfer
     self.sender.valid? && self.receiver.valid?
   end 
 
-  def execute_transaction 
-    if !self.valid? || self.sender.balance < self.amount
-        self.status = "rejected"
-        return "Transaction rejected. Please check your account balance."
-    elsif self.valid? && self.status == "pending"
-          self.sender.balance -= self.amount
-          self.receiver.balance += self.amount
-          self.status = "complete"
-    end
+  # def execute_transaction 
+  #   if !self.valid? || self.sender.balance < self.amount
+  #       self.status = "rejected"
+  #       return "Transaction rejected. Please check your account balance."
+  #   elsif self.valid? && self.status == "pending"
+  #         self.sender.balance -= self.amount
+  #         self.receiver.balance += self.amount
+  #         self.status = "complete"
+  #   end
+  # end 
+
+  def reject_transfer 
+    self.status = "rejected"
+    "Transaction rejected. Please check your account balance."
   end 
 
+  def execute_transaction
+    if self.valid? && self.sender.balance > self.amount && self.status == "pending"
+        self.sender.balance -= self.amount
+        self.receiver.balance += self.amount
+        self.status = "complete"
+    else 
+      reject_transfer
+    end
+  end
+
   def reverse_transfer
-    if self.status == "complete"
+    if self.valid? && self.receiver.balance > self.amount && self.status == "complete"
       self.sender.balance += self.amount 
       self.receiver.balance -= self.amount
       self.status = "reversed"
+    else 
+      reject_transfer
     end 
   end 
 
